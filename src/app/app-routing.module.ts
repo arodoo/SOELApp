5be7d947bcfeb7dd58e3342fc0823f8552e1a1ff
arodoo <haro.em@hotmail.com>
2023-04-19 11:20:32 -0600

    backup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ObrasComponent } from './components/obras/obras.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
+  { path: 'obras', component: ObrasComponent},
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
